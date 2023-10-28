12e6e5d5775f3e0e4fd40c3d2c5f0a6c9470fa50
Stefan Dramicanin <dramicanin.stefan@gmail.com>
2023-10-28 20:49:39 +0200

    redux

---
+++ Redux/redux-basic/src/App.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react'
+import { useSelector } from 'react-redux'
+
+import Header from './components/Header'
+import Auth from './components/Auth'
+import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
+
+function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated)
+
+  return (
+    <Fragment>
+      <Header></Header>
+      {isAuth ? <UserProfile></UserProfile> : <Auth></Auth>}
+      <Counter />
+    </Fragment>
+  )
+}
+
+export default App
