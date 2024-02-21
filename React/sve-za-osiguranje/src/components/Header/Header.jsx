b8d8d8dc0423845a21f6e70f511d5c49d26f6444
Stefan Dramicanin <dramicanin.stefan@gmail.com>
2024-02-21 15:38:42 +0100

    commit

--- React/sve-za-osiguranje/src/components/Header/Header.jsx
+++ React/sve-za-osiguranje/src/components/Header/Header.jsx
@@ -17,6 +17,15 @@ const Header = (props) => {
     setVehicles(false);
   };
 
+  // show vehicle mobile dropdown
+  const [vehiclesMobile, setVehiclesMobile] = useState(false);
+
+  const showVehiclesDropdownMobile = () => {
+    setVehiclesMobile((current) => !current);
+    setDamageMobile(false);
+    setHealthMobile(false);
+  };
+
   // show health dropdown
   const [health, setHealth] = useState(false);
 
@@ -28,6 +37,15 @@ const Header = (props) => {
     setHealth(false);
   };
 
+  // show health mobile dropdown
+  const [healthMobile, setHealthMobile] = useState(false);
+
+  const showHealthDropdownMobile = () => {
+    setVehiclesMobile(false);
+    setHealthMobile((current) => !current);
+    setDamageMobile(false);
+  };
+
   // show damage dropdown
   const [damage, setDamage] = useState(false);
 
@@ -37,6 +55,15 @@ const Header = (props) => {
 
   const hideDamageDropdown = () => {
     setDamage(false);
+  };
+
+  // show damage mobile dropdown
+  const [damageMobile, setDamageMobile] = useState(false);
+
+  const showDamageDropdownMobile = () => {
+    setVehiclesMobile(false);
+    setHealthMobile(false);
+    setDamageMobile((current) => !current);
   };
 
   // change color of header on scroll
@@ -51,16 +78,38 @@ const Header = (props) => {
 
   window.addEventListener('scroll', changeColor);
 
+  // open menu navbar
+
+  const [openMobileNav, setOpenMobileNav] = useState(false);
+
+  const handleMenuNav = () => {
+    setOpenMobileNav((current) => !current);
+    setVehiclesMobile(false);
+    setDamageMobile(false);
+    setHealthMobile(false);
+    window.scrollTo(0, 0);
+  };
+
+  // close menu navbar
+
+  const handleCloseMenuNav = () => {
+    setOpenMobileNav(false);
+    setVehiclesMobile(false);
+    setDamageMobile(false);
+    setHealthMobile(false);
+  };
+
   return (
     <Fragment>
       {/* <header className={color ? `${classes['wrapper']} ${classes.scroll}` : classes['wrapper']}> */}
-      <header className={classes['wrapper']} style={color ? { backgroundColor: `var(${props.backgroundColorScroll})` } : { backgroundColor: `var(${props.backgroundColor})` }}>
+      <div className={classes['wrapper']} style={color ? { backgroundColor: `var(${props.backgroundColorScroll})` } : { backgroundColor: `var(${props.backgroundColor})` }}>
         <div className={classes.container}>
           <div className={classes['header-logo']}>
             <Link to="/">
               <img src={logo} alt="SveZaOsiguranjeLogo" />
             </Link>
           </div>
+
           <div className={classes['header-content']}>
             <div className={classes['header-info']}>
               <div className={classes['header-info-news']}></div>
@@ -176,8 +225,130 @@ const Header = (props) => {
               </a>
             </div>
           </div>
+
+          <div className={classes.hamburgerMenu}>
+            <input className={classes.checkbox} id="checkbox22" type="checkbox" onClick={handleMenuNav} checked={openMobileNav} onChange={(current) => !current} />
+            <label className={`${classes.toggle}`} htmlFor="checkbox22">
+              <div className={`${classes.bar1} ${classes.bars}`}></div>
+              <div className={`${classes.bar2} ${classes.bars}`}></div>
+              <div className={`${classes.bar3} ${classes.bars}`}></div>
+            </label>
+          </div>
         </div>
-      </header>
+      </div>
+
+      {/* mobile navbar */}
+      <div className={openMobileNav ? `${classes['mobileMenu']} ${classes.active}` : classes['mobileMenu']}>
+        <ul className={classes.links}>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/" end>
+              Početna
+            </NavLink>
+          </li>
+          <li>
+            <div onClick={showVehiclesDropdownMobile}>
+              <p>
+                Vozila <i style={vehiclesMobile ? { transform: 'rotate(0deg)' } : { transform: 'rotate(-90deg)' }} className="fa-solid fa-angle-down"></i>
+              </p>
+              <ul className={vehiclesMobile ? `${classes['dropdown-vehicle']} ${classes.active}` : classes['dropdown-vehicle']}>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/kasko-osiguranje-vozila" end>
+                    Kasko osiguranje vozila
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/autoodgovornost" end>
+                    Autoodgovornost
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/pomoc-na-putu" end>
+                    Pomoc na putu
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/registracija-vozila" end>
+                    Registracija vozila
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/osiguranje-domacinstva" end>
+              Domaćinstvo
+            </NavLink>
+          </li>
+          <li>
+            <div onClick={showHealthDropdownMobile}>
+              <p>
+                Životno i zdravstveno <i style={healthMobile ? { transform: 'rotate(0deg)' } : { transform: 'rotate(-90deg)' }} className="fa-solid fa-angle-down"></i>
+              </p>
+              <ul className={healthMobile ? `${classes['dropdown-health']} ${classes.active}` : classes['dropdown-health']}>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/zivotno-osiguranje" end>
+                    Životno osiguranje
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/dobrovoljno-zdravstveno-osiguranje" end>
+                    Dobrovoljno zdravstveno <br /> osiguranje
+                  </NavLink>
+                </li>
+
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/osiguranje-od-nezgode" end>
+                    Osiguranje od nezgode
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/putno-osiguranje" end>
+              Putno
+            </NavLink>
+          </li>
+          <li>
+            <div onClick={showDamageDropdownMobile}>
+              <p>
+                Naplata štete <i style={damageMobile ? { transform: 'rotate(0deg)' } : { transform: 'rotate(-90deg)' }} className="fa-solid fa-angle-down"></i>
+              </p>
+              <ul className={damageMobile ? `${classes['dropdown-damage']} ${classes.active}` : classes['dropdown-damage']}>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/prijava-i-naknada-stete" end>
+                    Prijava i naknada štete
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/naplata-naknada-stete-na-vozilu" end>
+                    Naplata štete na vozilu
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/naknada-stete-kasko-osiguranje" end>
+                    Naknada stete kasko <br /> osiguranja
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/osiguranje-putnika-u-javnom-prevozu" end>
+                    Osiguranje putnika u <br />
+                    javnom prevozu
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className={({ isActive }) => (isActive ? classes.active : undefined)} to="/naknada-stete-za-fizicki-i-dusevni-bol" end>
+                    Naknada štete za fizičku <br /> i duševnu bol
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li onClick={handleCloseMenuNav}>
+            <a href="#contact-form">Kontakt</a>
+          </li>
+        </ul>
+      </div>
     </Fragment>
   );
 };
