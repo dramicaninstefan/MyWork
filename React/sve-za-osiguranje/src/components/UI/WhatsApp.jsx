bd631e59b8984405dbe8b045bf7f8fae48102adf
Stefan Dramicanin <dramicanin.stefan@gmail.com>
2024-03-06 12:21:02 +0100

    sve-za-osiguranje

--- React/sve-za-osiguranje/src/components/UI/WhatsApp.jsx
+++ React/sve-za-osiguranje/src/components/UI/WhatsApp.jsx
@@ -5,7 +5,7 @@ import classes from './WhatsApp.module.css';
 const WhatsApp = () => {
   return (
     <Fragment>
-      <a href="viber://chat/?number=%2B381638489439" className={classes.whatsAppBtn}>
+      <a href="https://wa.me/+381638489439" className={classes.whatsAppBtn}>
         <div className={classes.circle}>
           <i className="fa-brands fa-whatsapp"></i>
         </div>
