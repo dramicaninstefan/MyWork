bbd7fb9abc9181646c7306db89fea5fbff17ea4d
dramicaninstefan <dramicanin.stefan@gmail.com>
2025-02-07 23:19:43 +0100

    sdk php

--- React/sdkonsulting/src/components/UI/Preloader/Preloader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from './Preloader.module.css';
-
-const Preloader = () => {
-  return <div id={classes.preloader}></div>;
-};
-
-export default Preloader;
